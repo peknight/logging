c8f49cb64726eb4c6bd1b3ea7f840387677ba1f1
peknight <JKpeknight@gmail.com>
2025-03-23 17:55:20 +0800

    update

--- logging-core/shared/src/main/scala/com/peknight/logging/message/package.scala
+++ logging-core/shared/src/main/scala/com/peknight/logging/message/package.scala
@@ -4,7 +4,7 @@ import cats.Show
 import cats.syntax.show.*
 import com.peknight.error.{Error, Success}
 
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration.*
 
 package object message:
   def apply[A: Show, Param: Show](either: Either[Error, A],
@@ -15,5 +15,14 @@ package object message:
                                   param: Option[Param] = None): String =
     val error = either.fold(Error.apply, _ => Success)
     val value = either.toOption
-    s"$traceId|$name|${duration.map(_.toString).getOrElse("")}|${error.errorType}|${error.message}|${error.showValue.getOrElse("")}|${param.map(_.show).getOrElse("")}|${value.map(_.show).getOrElse("")}|$message"
+    s"$traceId|$name|${duration.map(format).getOrElse("")}|${error.errorType}|${error.message}|${error.showValue.getOrElse("")}|${param.map(_.show).getOrElse("")}|${value.map(_.show).getOrElse("")}|$message"
+
+  private def format(duration: Duration): String =
+    if duration > 10.days then s"${duration.toDays}days#${duration.toNanos}"
+    else if duration > 10.hours then s"${duration.toHours}hours#${duration.toNanos}"
+    else if duration > 10.minutes then s"${duration.toMinutes}minutes#${duration.toNanos}"
+    else if duration > 10.seconds then s"${duration.toSeconds}seconds#${duration.toNanos}"
+    else if duration > 10.millis then s"${duration.toMillis}millis#${duration.toNanos}"
+    else if duration > 10.micros then s"${duration.toMicros}micros#${duration.toNanos}"
+    else s"${duration.toNanos}ns#${duration.toNanos}"
 end message
